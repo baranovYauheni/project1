e0882209ea6ca98dbbe86e60dd2e51ced07cb949
Yauheni <baranov.yauheni@icloud.com>
2020-08-31 01:35:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// const result = confirm("Are you hear?");
+
+const answers = [];
+
+answers[0] = prompt('How old are you?');
+answers[1] = prompt('whats is your name?');
+answers[2] = prompt('do you speak english?');
+
+document.write(answers);
